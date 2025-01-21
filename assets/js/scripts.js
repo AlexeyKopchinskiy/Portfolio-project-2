d3c1a8531b34bda52e41a1c83640a6c5edc1af77
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-01-21 16:33:36 +0100

    Check that the function is initiated on page load

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,8 @@
 // Function that gets the quiz content from the forms on the page
 // and displays the quiz score
-function getQuizContent() {}
+function getQuizContent() {
+	console.log("getQuizContent() called");
+}
 
 // function to initiate the quiz when the page loads
 document.addEventListener("DOMContentLoaded", getQuizContent());
