d8eb0c190dde51967c8a2a439ac78b5b82898740
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-01-21 16:38:12 +0100

    Check whether loading has been completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,5 +4,12 @@ function getQuizContent() {
 	console.log("getQuizContent() called");
 }
 
-// function to initiate the quiz when the page loads
-document.addEventListener("DOMContentLoaded", getQuizContent());
+// Event to initiate the quiz when the page loads
+// But first check whether loading has been completed
+if (document.readyState === "loading") {
+	// Loading isn't finished
+	document.addEventListener("DOMContentLoaded", getQuizContent);
+} else {
+	// `DOMContentLoaded` has been fired
+	getQuizContent();
+}
