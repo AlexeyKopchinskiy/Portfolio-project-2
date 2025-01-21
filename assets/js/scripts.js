d4253b7c209f4812adb5c8004daa2c3cd2aeff72
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-01-21 16:41:53 +0100

    Set initial variables

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,12 @@
 // Function that gets the quiz content from the forms on the page
 // and displays the quiz score
 function getQuizContent() {
-	console.log("getQuizContent() called");
+	// console.log("getQuizContent() called"); // Check if the function is called
+	// Set list of quiz types and initial score variables
+	const quizzes = ["biology", "astronomy", "geography", "history"]; // List of quiz types
+	let totalScore = 0; // Total score of the user
+	let quizzesCompleted = 0; // Number of quizzes completed
+	let maxPossibleScore = 0; // Maximum possible score
 }
 
 // Event to initiate the quiz when the page loads
