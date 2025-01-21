c247aab53e0ad28a7086538dbd9e843f4b221edc
Alexey Kopchinskiy <o68pl5n0@students.codeinstitute.net>
2025-01-21 16:22:39 +0100

    Set the initial structure of the main quiz functions in JS file

---
+++ assets/js/scripts.js
@@ -0,0 +1,6 @@
+// Function that gets the quiz content from the forms on the page
+// and displays the quiz score
+function getQuizContent() {}
+
+// function to initiate the quiz when the page loads
+document.addEventListener("DOMContentLoaded", getQuizContent());
